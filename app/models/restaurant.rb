5cc0de010202b8d39d0b2dd0b09d3fba1a4f5a4e
CarsonRey <c.rey494@gmail.com>
2018-10-18 10:49:08 -0400

    AMAZING

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,47 @@
 class Restaurant
+
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      name == restaurant.name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    reviews.collect{|review| review.customer}.uniq
+  end
+
+  def longest_review
+    reviews.max do |reviewA, reviewB|
+    reviewA.content.length <=> reviewB.content.length
+    end
+  end
+
+  def ratings
+    reviews.collect {|review| review.rating}
+  end
+
+  def average_star_rating
+    ratings.reduce(:+)/ratings.length
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,37 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+carson = Customer.new("Carson", "Rey")
+lili = Customer.new("Lili", "Finkel")
+
+zoe = Customer.new("Zoe", "Schwartz")
+
+kaitlin = Customer.new("Kaitlin", "Gu")
+
+other_zoe = Customer.new("Zoe", "Baker-Peng")
+
+chinese = Restaurant.new("China Res")
+italian = Restaurant.new("Italy Res")
+indian = Restaurant.new("Indian Res")
+halal = Restaurant.new("Halal Res")
+
+other_halal = Restaurant.new("Halal Res")
+
+
+review1 = Review.new(carson, italian, 5, "It was great!")
+
+review2 = Review.new(carson, italian, 5, "It was still great! This restaurant is super consistent when it comes to quality and I appreciate it!")
+
+review1 = Review.new(carson, indian, 4, "It was good!")
+
+review3 = Review.new(lili, indian, 4, "It was good!")
+
+review4 = Review.new(kaitlin, halal, 3, "It was okay.")
+
+review5 = Review.new(zoe, chinese, 2, "Eh..")
+
+review6 = Review.new(zoe, italian, 2, "WHAT ITALIAN RESTAURANT DOESN'T HAVE AN EXTENSIVE WINE LIST??")
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
